abe05fa1c59b0af13d0b213e04c4834bbf725a3b
maraa <mara.arthie@gmail.com>
2017-05-27 07:46:30 +0000

    fixed typo in license in metadata.rb, fixes FC069,FC078.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer        'rocketchat'
 maintainer_email  'cookbook@rocket.chat'
-license           'Apache 2'
+license           'Apache 2.0'
 description       'Installs/Configures Rocket.Chat'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.2.0'
